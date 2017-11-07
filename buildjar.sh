ca8fd90eb145c797c82ced6bd5034a3cbd22709d
Chris Decelles <cdecelles@pivotal.io>
2017-11-07 16:11:00 -0500

    initial commit

---
+++ buildjar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm cjd.war
+#rm WEB-INF/lib/pcf-hea*.jar
+#cd PCFHealthCheck
+#mvn clean package
+#cd ..
+
+cp PCFHealthCheck/target/pcf-healthcheck-1.jar WEB-INF/lib/.
+jar cvf cjd.war META-INF/ WEB-INF/ docs scripts
+
+cf push -f manifest-healthcheck.yml
