b316d5f678419817f08f2620ca2a59994c5bb128
Gyan <pu.gyan@gmail.com>
2017-11-08 19:02:06 -0500

    Update HealthCheckServlet.java
    added scanner change

--- src/main/java/io/pivotal/healthcheck/HealthCheckServlet.java
+++ src/main/java/io/pivotal/healthcheck/HealthCheckServlet.java
@@ -26,61 +26,84 @@ import org.xml.sax.InputSource;
 public class HealthCheckServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -7040738151776303470L;
+	
+	public static final Map<String, String> env = 	System.getenv();
+	public static final String KIE_CONTAINER 	= 	env.get("KIE_CONTAINER");
+	public static final String KIE_SCANNER 		= 	env.get("KIE_SCANNER");
+	public static final String KIE_CONTAINER_LIST = "http://localhost:8080/services/rest/server/containers";
+	public static final String KIE_SERVER_AUTH	= 	"Basic a2lldXNlcjpwYXNzd29yZDE=";
+	
+	public static Document kieContainer = null;
+	public static Document kieScanner = null;
 
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("Servlet " + this.getServletName() + " has started");
+		System.out.println("##### Loading Container and Scanner config from Env. ###########################\n");
+
+		try {
+			kieContainer = loadXML(KIE_CONTAINER);
+			if(KIE_SCANNER != null && !KIE_SCANNER.isEmpty() ){
+			   kieScanner = loadXML(KIE_SCANNER);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	@Override
+	public void destroy() {
+		System.out.println("Servlet " + this.getServletName() + " has stopped");
+	}
+
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		System.out.println("##### START OF HEALTHCHECK ###########################\n");
-
-		Map<String, String> env = System.getenv();
-		String KIE_CONTAINER = env.get("KIE_CONTAINER");
-
-		System.out.println("##### KIE_CONTAINER=" + KIE_CONTAINER + "\n");
-		Document kieContainer = null;
-		try {
-			kieContainer = loadXML(KIE_CONTAINER);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		String returnString = "PCF Health Check ";
+		
 		String kcid = kieContainer.getFirstChild().getAttributes().getNamedItem("container-id").getNodeValue();
 
 		try {
-			Document d = listContainers();
+			 Document d = listContainers();
 
 			if (!doesContainerExist(d, kcid)) {
 				System.out.println("##### container does not exist ###########################\n");
 
 				createContainer(kcid, KIE_CONTAINER);
+			
+				if(KIE_SCANNER != null && !KIE_SCANNER.isEmpty()){
+					createScanner(kcid, KIE_SCANNER);
+				}
 				response.sendError(503);
+
 			} else {
 				System.out.println("##### container exist ###########################\n");
-
-				if (!isContainerStarted(d, kcid))
+				if (!isContainerStarted(d, kcid)){
 					response.sendError(503);
+				} 
+				// TODO: is required to monitor scanner ?
+				/*else {
+					if(!isScannerStarted(d, kcid)){
+						createScanner(kcid, KIE_SCANNER);
+					}
+				} 	*/				
 			}
 
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		System.out.println("##### END OF HEALTHCHECK ###########################");
 
 		response.getWriter().println(returnString);
 	}
-	
-	@Override
-	public void init() throws ServletException {
-		System.out.println("Servlet " + this.getServletName() + " has started");
-	}
 
-	@Override
-	public void destroy() {
-		System.out.println("Servlet " + this.getServletName() + " has stopped");
-	}
+
 
 	private String getAuthorizationString() {
 		String username = "kieuser";
@@ -92,37 +115,20 @@ public class HealthCheckServlet extends HttpServlet {
 		return "Basic " + basicAuth;
 	}
 
+	
+	
 	private String createContainer(String kcid, String container) throws Exception {
-		System.out.println("!!!!!!!!!!!!######## create container START");
+		System.out.println("######## create container START");
 
+		String responsestring = "";
+		
 		Document d = listContainers();
 
-		// if (!isContainerStarted(d, "hello")) {
-		//
-		// }
-
-		// # Create Container command
-		// CONTAINER_CREATE_COMMAND="curl -X PUT -H \"$AUTH_KIE\" -H
-		// \"$X_CF_APP_INSTANCE\" -H \"$CONTENT_TYPE\" -d
-		// \"$CONTAINER_CREATE_DATA\" $APP_CNTR_CREATION_URL"
-		
-		//# Request Body data
-		// String container="<kie-container
-		// container-id=\"hr\"><release-id><artifact-id>guvnor-asset-mgmt-project</artifact-id><group-id>org.guvnor</group-id><version>6.5.0.Final</version></release-id></kie-container>";
-
-		String urlString = "http://localhost:8080/services/rest/server/containers/" + kcid;
-		String responsestring = "";
-
+		String urlString = KIE_CONTAINER_LIST + "/" + kcid;
 		URL url = new URL(urlString);
 		HttpURLConnection c = (HttpURLConnection) url.openConnection();
-		// HttpsURLConnection c = (HttpsURLConnection) url.openConnection();
 
-		String username = "kieuser";
-		String password = "password1";
-
-		String basicAuth = Base64.getEncoder()
-				.encodeToString((username + ":" + password).getBytes(StandardCharsets.UTF_8));
-		c.setRequestProperty("Authorization", "Basic a2lldXNlcjpwYXNzd29yZDE=");
+		c.setRequestProperty("Authorization", KIE_SERVER_AUTH );
 		c.setRequestProperty("Content-Type", "application/xml");
 		c.setUseCaches(false);
 		c.setDoInput(true);
@@ -140,23 +146,71 @@ public class HealthCheckServlet extends HttpServlet {
 		while ((str = in.readLine()) != null) // reading data
 			responsestring += str + "\n";// process the response and save it in
 
-		// System.out.println("######## create container results=" +
-		// responsestring);
-
+		System.out.println("######## create container results=r" + responsestring);
 		Document doc = loadXML(responsestring);
 
-		return container;
+		return responsestring;
 	}
 
-	private Document listContainers() throws Exception {
-		String urlString = "http://localhost:8080/services/rest/server/containers";
-		String responsestring = "";
+	
+	/**
+	 *  Add scanner to given container
+	 * @param kcid
+	 * @param kieScanner
+	 */
+	private String createScanner(String kcid, String kieScannerBody)  throws Exception{
+		
+		System.out.println("######## create Scanner START");
 
+		String responsestring = "";
+		
+		Document d = listContainers();
+
+		String urlString = KIE_CONTAINER_LIST + "/" + kcid + "/scanner";
 		URL url = new URL(urlString);
 		HttpURLConnection c = (HttpURLConnection) url.openConnection();
-		c.setRequestProperty("Authorization", "Basic a2lldXNlcjpwYXNzd29yZDE=");
 
+		c.setRequestProperty("Authorization", KIE_SERVER_AUTH );
+		c.setRequestProperty("Content-Type", "application/xml");
+		c.setUseCaches(false);
+		c.setDoInput(true);
+		c.setDoOutput(true);
+		c.setRequestMethod("POST");
+
+		// Send request
+		DataOutputStream wr = new DataOutputStream(c.getOutputStream());
+		wr.writeBytes(kieScannerBody);
+		wr.flush();
+		wr.close();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+		String str;
+		while ((str = in.readLine()) != null) // reading data
+			responsestring += str + "\n";// process the response and save it in
+
+		System.out.println("######## create container results=r" + responsestring);
+		Document doc = loadXML(responsestring);
+
+		return responsestring;
+		
+		
+	}
+	
+	
+	/**
+	 * Get list of present contianers
+	 * @return
+	 * @throws Exception
+	 */
+	private Document listContainers() throws Exception {
+	
+		String responsestring = "";
+
+		URL url = new URL(KIE_CONTAINER_LIST);
+		HttpURLConnection c = (HttpURLConnection) url.openConnection();
+		c.setRequestProperty("Authorization", KIE_SERVER_AUTH );
 		c.setRequestMethod("GET");
+		
 		BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
 		String str;
 		while ((str = in.readLine()) != null) // reading data
@@ -168,8 +222,15 @@ public class HealthCheckServlet extends HttpServlet {
 		return loadXML(responsestring);
 	}
 
-	private Document loadXML(String xml) throws Exception {
-
+	
+	
+	/**
+	 * Convert  String xml content to XML Document
+	 * @param xml
+	 * @return
+	 * @throws Exception
+	 */
+	private static Document loadXML(String xml) throws Exception {
 		System.out.println("#################### start loadXML ###########################");
 		System.out.println("####################" + xml);
 
@@ -179,10 +240,17 @@ public class HealthCheckServlet extends HttpServlet {
 		Document d = bldr.parse(insrc);
 
 		System.out.println("#################### end loadXML ###########################");
-
 		return d;
 	}
+	
+	
 
+	/**
+	 * Check for given container-id exits
+	 * @param document
+	 * @param cid
+	 * @return
+	 */
 	private boolean doesContainerExist(Document document, String cid) {
 		NodeList kieContainers = document.getElementsByTagName("kie-containers");
 
@@ -222,6 +290,7 @@ public class HealthCheckServlet extends HttpServlet {
 		return false;
 	}
 
+	
 	private boolean isContainerStarted(Document document, String cid) {
 		NodeList kieContainers = document.getElementsByTagName("kie-containers");
 
@@ -261,7 +330,57 @@ public class HealthCheckServlet extends HttpServlet {
 
 		return false;
 	}
+	
 
+	
+	
+	
+	
+	private boolean isScannerStarted(Document document, String kcid) {
+
+		NodeList kieContainers = document.getElementsByTagName("kie-containers");
+
+		// there is only 1 kie-containers elements
+		System.out.println("#### isScannerStarted kieContainers.getLength()=" + kieContainers.getLength());
+
+		for (int i = 0; i < kieContainers.getLength(); i++) {
+			
+			Node container = kieContainers.item(i);
+
+			// are there are kie-container elements, if not return false
+			if (!container.hasChildNodes())
+				return false;
+			
+			// get the list of kie-container elements
+			NodeList nodes = container.getChildNodes();
+
+			for (int k = 0; k < nodes.getLength(); k++) {
+
+				// get the kie-container element and check the attribute for container id 
+				Node subNode = nodes.item(k);
+				String containerID = subNode.getAttributes().getNamedItem("container-id").getNodeValue();
+				
+				NodeList innerNodes = subNode.getChildNodes();
+				//loop through container 
+				for(int s= 0; s < innerNodes.getLength(); s++){
+					Node innerNode = innerNodes.item(s);
+					String scannerStatus = innerNode.getAttributes().getNamedItem("status").getNodeValue();
+					System.out.println("##### isScannerStarted check of container and status: " + containerID
+							+ " scanner status: " + scannerStatus);
+					if(scannerStatus!= null && scannerStatus.equalsIgnoreCase("STARTED") 
+							&& containerID != null &&  containerID.equalsIgnoreCase(kcid)){
+						return true;
+						
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	
+	
 	private String getContainerStatus(Document document, String cid) {
 		String status = null;
 		NodeList kieContainers = document.getElementsByTagName("kie-containers");
